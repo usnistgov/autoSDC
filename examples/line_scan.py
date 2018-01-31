7e6979c1dd0b735748d2cf54e67308591c15a023
Brian DeCost <brian.decost@nist.gov>
2018-01-31 11:28:37 -0500

    update position steps -- units are meters!

--- examples/line_scan.py
+++ examples/line_scan.py
@@ -2,22 +2,30 @@
 
 import json
 import time
-import datetime
+from datetime import datetime
+
 import versastat.position
 import versastat.control
 
-def line_scan(speed=0.0001, poll_interval=0.5):
-    """ perform a line scan with CV experiments, recording position, current, potential, and parameters in json log files """
+def line_scan(speed=1e-5, poll_interval=0.5):
+    """ perform a line scan with CV experiments, recording position, current, potential, and parameters in json log files
+    Position units are METERS!
+    """
 
-    delta = [0.001, 0.001, 0.0]
+    delta = [1e-4, 1e-4, 0.0]
+    initial_delta = [0.0, 0.0, -2.60e-4]
+    n_steps = 10
 
     with versastat.position.Position(ip='192.168.10.11', speed=speed) as pos:
-        
+
+        pos.print_status()
+        pos.update(delta=initial_delta, verbose=True)
         pos.print_status()
 
         ctl = versastat.control.Control(start_idx=17109013)
 
-        for idx in range(10):
+        for idx in range(n_steps):
+            # scan, log, take a position step
 
             # run a CV experiment
             status, params = ctl.cyclic_voltammetry()
@@ -25,13 +33,13 @@ def line_scan(speed=0.0001, poll_interval=0.5):
 
             while ctl.sequence_running():
                 time.sleep(poll_interval)
-                
+
             # collect and log data
             scan_data = {
                 'measurement': 'cyclic_voltammetry',
                 'parameters': params,
                 'index_in_sequence': idx,
-                'timestamp': datetime.datetime.now().isoformat(),
+                'timestamp': datetime.now().isoformat(),
                 'current': ctl.current(),
                 'potential': ctl.potential(),
                 'position': pos.current_position()
@@ -42,7 +50,7 @@ def line_scan(speed=0.0001, poll_interval=0.5):
                 json.dump(scan_data, f)
 
             ctl.clear()
-            
+
             # update position
             pos.update(delta=delta, verbose=True)
             pos.print_status()
