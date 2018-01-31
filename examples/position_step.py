79ea9bf8856802e9627f7680887363545626323b
Brian DeCost <brian.decost@nist.gov>
2018-01-31 13:59:04 -0500

    update position_status example to use the new context manager, and use better default step size.

--- examples/position_step.py
+++ examples/position_step.py
@@ -2,12 +2,12 @@
 
 import versastat.position
 
-def position_step(speed=0.0001):
+def position_step(speed=1e-5):
 
-    with versastat.position.Position(ip='192.168.10.11', speed=speed) as pos:
+    with versastat.position.position(ip='192.168.10.11', speed=speed) as pos:
         
         pos.print_status()
-        pos.update_x(delta=0.001, verbose=True)
+        pos.update_x(delta=1e-4, verbose=True)
         pos.print_status()
     
 
