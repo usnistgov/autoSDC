fc097c87ff9509461fa88e050e1587795fe86038
Brian DeCost <brian.decost@nist.gov>
2018-10-22 15:46:24 -0400

    add CV details.

--- asdc/scripts/combi_cv.py
+++ asdc/scripts/combi_cv.py
@@ -1,14 +1,84 @@
-import click
+#!/usr/bin/env python
+import os
+import sys
+import glob
+import json
 import time
+import click
 import numpy as np
-import pandas as pd
+from datetime import datetime
+import matplotlib.pyplot as plt
 
+import asdc.control
 import asdc.position
 
-def run_cv_scan():
+def plot_iv(I, V, idx, data_dir='data'):
+    plt.plot(np.log10(np.abs(I)), V)
+    plt.xlabel('log current')
+    plt.ylabel('voltage')
+    plt.savefig(os.path.join(data_dir, 'iv_{}.png'.format(idx)))
+    return
+
+def run_cv_scan(scan_idx, cell='INTERNAL', data_dir='data', verbose=False, initial_delay=30):
     """ run a CV scan for each point """
-    print('collect CV data here...')
-    time.sleep(3)
+
+    # check on experiment status periodically:
+    poll_interval = 1
+    time.sleep(initial_delay)
+
+    with asdc.position.controller(ip='192.168.10.11') as pos:
+
+        with asdc.control.controller(start_idx=17109013) as pstat:
+
+            # run an open-circuit followed by a CV experiment
+            status, oc_params = pstat.open_circuit(
+                time_per_point=1, duration=60, current_range='AUTO', e_filter='1Hz', i_filter='1Hz'
+            )
+
+            if verbose:
+                print('OC added.')
+                print(status)
+                print(oc_params)
+
+            status, params = pstat.multi_cyclic_voltammetry(
+                initial_potential=0.0, vertex_potential_1=-1.0, vertex_potential_2=1.0, final_potential=0.0, scan_rate=0.075,
+                cell_to_use=cell, e_filter='1Hz', i_filter='1Hz', cycles=1
+            )
+
+            if verbose:
+                print('CV added.')
+                print(status)
+                print(params)
+
+            pstat.start()
+
+            error_codes = set()
+            while pstat.sequence_running():
+                time.sleep(poll_interval)
+                overload_status = pstat.overload_status()
+                if overload_status != 0:
+                    error_codes.add(overload_status)
+
+            # collect and log data
+            scan_data = {
+                'measurement': 'cyclic_voltammetry',
+                'parameters': params,
+                'index_in_sequence': scan_idx,
+                'timestamp': datetime.now().isoformat(),
+                'current': pstat.current(),
+                'potential': pstat.potential(),
+                'position': pos.current_position(),
+                'error_codes': error_codes
+            }
+
+            logfile = 'grid_scan_{:03d}.json'.format(scan_idx)
+            with open(os.path.join(data_dir, logfile), 'w') as f:
+                json.dump(scan_data, f)
+
+            plot_iv(scan_data['current'], scan_data['potential'], scan_idx, data_dir)
+
+            pstat.clear()
+
     return
 
 @click.command()
@@ -50,7 +120,7 @@ def run_combi_scan(target_file, data_dir, delta_z, speed, cell, verbose):
             pos.update(delta=delta)
 
         # run CV scan
-        run_cv_scan()
+        run_cv_scan(idx, cell, data_dir=data_dir, verbose=verbose, initial_delay=30)
 
     # go back to the original position....
     with asdc.position.controller(ip='192.168.10.11', speed=speed) as pos:
