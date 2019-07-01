75528f03dcbda37671e0cd02dc48378f989d0138
Brian DeCost <brian.decost@nist.gov>
2019-07-01 17:13:33 -0400

    initial pyro GP implementation.

--- asdc/emulation.py
+++ asdc/emulation.py
@@ -1,7 +1,12 @@
-import gpflow
+import torch
+from torch import optim
 import dataset
 import numpy as np
 import pandas as pd
+
+import pyro
+import pyro.contrib.gp as gp
+import pyro.distributions as dist
 
 def simplex_grid(n=3, buffer=0.1):
     """ construct a regular grid on the ternary simplex """
@@ -18,50 +23,58 @@ def simplex_grid(n=3, buffer=0.1):
     s = buffer + s*scale
     return s
 
-def model_ternary(composition, target, reset_tf_graph=True, drop_last=True, optimize_noise_variance=True, initial_noise_var=1e-4):
+def update_posterior(model, x_new=None, y_new=None, num_steps=150):
+
+    if x_new is not None and y_new is not None:
+        X = torch.cat([model.X, x_new])
+        y = torch.cat([model.y, y_new.squeeze(1)])
+        model.set_data(X, y)
+
+    # update model noise prior based on variance of observed data
+    model.set_prior('noise', dist.HalfNormal(model.y.var()))
+
+    # reinitialize hyperparameters from prior
+    p = model.kernel._priors
+    model.kernel.variance = p['variance']()
+    model.kernel.lengthscale = p['lengthscale'](model.kernel.lengthscale.size())
+    # model.noise = model._priors['noise']()
+
+    optimizer = optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=0.001)
+    losses = gp.util.train(model, optimizer, num_steps=num_steps)
+    return losses
+
+def model_ternary(X, y, drop_last=True, optimize_noise_variance=True, initial_noise_var=1e-4):
+    """ set up GP model for single target """
 
     if drop_last:
-        X = composition[:,:-1] # ignore the last composition column
-    else:
-        X = composition
-    Y = target
+        X = X[:,:-1] # ignore the last composition column
 
-    # sel = np.isfinite(Y).sum(axis=1)
-    sel = np.isfinite(Y).flat
-    X, Y = X[sel], Y[sel]
-    N, D = X.shape
+    sel = torch.isfinite(y)
+    X, y = X[sel], y[sel]
+    N, D = X.size()
 
-    if reset_tf_graph:
-        gpflow.reset_default_graph_and_session()
-
-    with gpflow.defer_build():
-        m = gpflow.models.GPR(
-            X, Y,
-            # kern=gpflow.kernels.Linear(D, ARD=True) + gpflow.kernels.RBF(D, ARD=True) + gpflow.kernels.Constant(D) + gpflow.kernels.White(D)
-            kern=gpflow.kernels.Matern52(D, ARD=True) + gpflow.kernels.Constant(D) + gpflow.kernels.White(D, variance=initial_noise_var) # \sigma_noise = 0.01
-            # kern=gpflow.kernels.RationalQuadratic(D, ARD=True) + gpflow.kernels.Constant(D) + gpflow.kernels.White(D, variance=initial_noise_var)
-        )
+    # set up ARD Matern 5/2 kernel
+    # set an empirical mean function to the median value of observed data...
+    kernel = gp.kernels.Matern52(input_dim=2, variance=torch.tensor(1.), lengthscale=torch.tensor([1.0, 1.0]))
+    model = gp.models.GPRegression(X, y, kernel, noise=torch.tensor(initial_noise_var))
+    model.mean_function = lambda x: model.y.median()
 
     # set a weakly-informative lengthscale prior
-    # e.g. half-normal(0, dx/3) -> gamma(0.5, 2*dx/3)
-    # another choice might be to use an inverse gamma prior...
-    # m.kern.kernels[0].lengthscales.prior = gpflow.priors.Gamma(0.5, 2.0/3)
-    m.kern.kernels[0].lengthscales.prior = gpflow.priors.Gamma(0.5, 0.5)
+    # e.g. half-normal(0, dx/3)
+    dx = 1.0
+    model.kernel.set_prior("lengthscale", dist.HalfNormal(dx/3))
+    model.kernel.set_prior("variance", dist.Gamma(2.0, 1/2.0))
 
-    # m.kern.kernels[0].variance.prior = gpflow.priors.Gamma(0.5, 4.)
-    # m.kern.kernels[1].variance.prior = gpflow.priors.Gamma(0.5, 4.)
-    # m.kern.kernels[2].variance.prior = gpflow.priors.Gamma(0.5, 2.)
-    m.kern.kernels[0].variance.prior = gpflow.priors.Gamma(2.0, 2.0)
-    m.kern.kernels[1].variance.prior = gpflow.priors.Gamma(2.0, 2.0)
-    # m.kern.kernels[2].variance.prior = gpflow.priors.Gamma(2.0, 2.0)
+    # set a prior on the likelihood noise based on the variance of the observed data
+    model.set_prior('noise', dist.HalfNormal(model.y.var()/2))
 
     if not optimize_noise_variance:
-        m.kern.kernels[2].variance.trainable = False
+        raise NotImplementedError
+        for name, param in model.named_parameters():
+            if name == 'noise_map_unconstrained':
+                param.requires_grad = False
 
-    m.likelihood.variance = 1e-6
-
-    m.compile()
-    return m
+    return model
 
 class ExperimentEmulator():
     def __init__(self, db_file, components=['Ni', 'Al', 'Ti'], targets = ['V_oc', 'I_p', 'V_tp', 'slope', 'fwhm'], optimize_noise_variance=True):
@@ -77,35 +90,74 @@ class ExperimentEmulator():
         # self.df = self.df.drop(45)
 
         self.components = components
+        self.composition = self.df.loc[:,self.components].values
         self.targets = targets
         self.optimize_noise_variance = optimize_noise_variance
 
         self.models = {}
+        self.samplers = {}
         self.fit()
 
     def fit(self):
-        self.composition = self.df.loc[:,self.components].values
 
-        self.opt = gpflow.training.ScipyOptimizer()
+        X = torch.tensor(self.composition, dtype=torch.float)
+
         for target in self.targets:
-            model = model_ternary(self.composition, self.df[target].values[:,None], optimize_noise_variance=self.optimize_noise_variance)
-            session = gpflow.get_default_session()
-            self.opt.minimize(model)
-            self.models[target] = (session, model)
+            y = torch.tensor(self.df[target].values, dtype=torch.float)
 
-    def __call__(self, composition, target=None, return_var=False, sample_posterior=False, n_samples=1, seed=None):
+            model = model_ternary(X, y, initial_noise_var=0.1, optimize_noise_variance=self.optimize_noise_variance)
+            update_posterior(model, num_steps=2000)
+            self.models[target] = model
+
+    def __call__(self, X, target=None, noiseless=True, sample_posterior=False, n_samples=1, seed=None):
         """ evaluate GP models on compositions """
-        session, model = self.models[target]
 
-        with session.as_default():
-            if sample_posterior:
-                if seed is not None:
-                    tf.set_random_seed(seed)
-                mu = model.predict_f_samples(composition[:,:-1], n_samples)
-                return mu.squeeze()
+        if target is None:
+            targets = self.models.keys()
+        elif type(target) is str:
+            targets = [target]
+
+        mean, var = {}, {}
+        for target in targets:
+            model = self.models[target]
+
+            with torch.no_grad():
+                _mean, _cov = model(X[:,:-1], full_cov=True, noiseless=noiseless)
+
+            mean[target] = _mean
+            var[target] = _cov.diag()
+
+        return mean, var
+
+        if sample_posterior:
+            if seed is not None:
+                tf.set_random_seed(seed)
+            mu = model.predict_f_samples(composition[:,:-1], n_samples)
+            return mu.squeeze()
+        else:
+            mu, var = model.predict_y(composition[:,:-1])
+            if return_var:
+                return mu, var
             else:
-                mu, var = model.predict_y(composition[:,:-1])
-                if return_var:
-                    return mu, var
-                else:
-                    return mu.squeeze()
+                return mu.squeeze()
+
+    def sample_posterior(self, X, target=None, noiseless=True):
+        """ sample GP posteriors """
+
+        if target is None:
+            targets = self.models.keys()
+        elif type(target) is str:
+            targets = [target]
+
+        mean = {}
+        for target in targets:
+            try:
+                fn = self.samplers[target]
+            except KeyError:
+                model = self.models[target]
+                fn = model.iter_sample(noiseless=noiseless)
+                self.samplers[target] = fn
+
+            mean[target] = fn(X[:,:-1])
+
+        return mean
