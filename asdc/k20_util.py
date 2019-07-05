6a804f15efb36416807f6517197509306b474cdb
Brian DeCost <brian.decost@nist.gov>
2019-07-05 10:36:01 -0400

    make model plotting function agnostic to the number of objectives...

--- asdc/k20_util.py
+++ asdc/k20_util.py
@@ -3,13 +3,19 @@ import matplotlib.pyplot as plt
 from asdc import visualization
 
 def plot_emulator(model, domain, sample_posterior=True, fig_path=None):
-    if sample_posterior:
-        fig, axes = plt.subplots(nrows=3, ncols=4, figsize=(16,12))
-    else:
-        fig, axes = plt.subplots(nrows=2, ncols=4, figsize=(16,8))
+    n_objectives = len(model.models)
 
-    features = ['V_oc', 'V_tp', 'I_p', 'slope']
-    labels = [r'$V_{oc}$', r'$V_{tp}$', r'log $I_p$', 'slope']
+    if sample_posterior:
+        fig, axes = plt.subplots(nrows=3, ncols=n_objectives, figsize=(4*n_objectives,12))
+    else:
+        fig, axes = plt.subplots(nrows=2, ncols=n_objectives, figsize=(4*n_objectives,8))
+
+    labels = {
+        'V_oc': r'$V_{oc}$',
+        'V_tp': r'$V_{tp}$',
+        'I_p': r'log $I_p$',
+        'slope': 'slope'
+    }
 
     mean, var = model(domain)
     if sample_posterior:
@@ -18,16 +24,17 @@ def plot_emulator(model, domain, sample_posterior=True, fig_path=None):
         Y_sample, noise_sample = model.clean_iter_sample(domain, uniform_noise=True)
         sample = {key: (Y_sample[key] + noise_sample[key]) for key in Y_sample.keys()}
 
-    for col, feature, title in zip(axes.T, features, labels):
-        m = model.models[feature]
+    for col, feature, m in zip(axes.T, model.models.items()):
+
         visualization.ternary_scatter_sub(m.X, m.y, ax=col[0])
         visualization.ternary_scatter_sub(domain.numpy(), mean[feature].numpy(), ax=col[1]);
+
         if sample_posterior:
             visualization.ternary_scatter_sub(domain.numpy(), sample[feature].numpy(), ax=col[2]);
 
         for ax in col:
             ax.axis('equal')
-        col[0].set_title(title, size=18)
+        col[0].set_title(labels[feature], size=18)
 
     plt.tight_layout()
     plt.subplots_adjust()
