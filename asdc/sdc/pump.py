45e44db9d806039a6b36396aee9ee46afb0d84df
Brian DeCost <brian.decost@nist.gov>
2019-07-24 15:20:11 -0400

    start a object-oriented interface for the pump array.

--- asdc/sdc/pump.py
+++ asdc/sdc/pump.py
@@ -2,10 +2,15 @@ from __future__ import print_function
 
 import serial
 
-def legato_check(port='COM7', baud=115200, timeout=1):
-    with serial.Serial(port=port, baudrate=baud, timeout=timeout) as ser:
-        print('connected')
-        ser.write('config\r\n'.encode())
-        s = ser.read(100)
-        print(s)
-    return
+class PumpArray():
+    def __init__(port='COM7', baud=115200, timeout=1):
+        self.port = port
+        self.baud = baud
+        self.timeout = 1
+
+    def print_config(selfport='COM7', baud=115200, timeout=1):
+        with serial.Serial(port=self.port, baudrate=self.baud, timeout=self.timeout) as ser:
+            print('connected')
+            ser.write('config\r\n'.encode())
+            s = ser.read(100)
+            print(s)
