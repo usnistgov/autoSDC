5ad283b9a6933039587147b494d607d18a25c9f1
Brian DeCost <brian.decost@nist.gov>
2019-07-24 15:20:59 -0400

    syntax errors.

--- asdc/sdc/pump.py
+++ asdc/sdc/pump.py
@@ -3,12 +3,12 @@ from __future__ import print_function
 import serial
 
 class PumpArray():
-    def __init__(port='COM7', baud=115200, timeout=1):
+    def __init__(self, port='COM7', baud=115200, timeout=1):
         self.port = port
         self.baud = baud
         self.timeout = 1
 
-    def print_config(selfport='COM7', baud=115200, timeout=1):
+    def print_config(self, port='COM7', baud=115200, timeout=1):
         with serial.Serial(port=self.port, baudrate=self.baud, timeout=self.timeout) as ser:
             print('connected')
             ser.write('config\r\n'.encode())
