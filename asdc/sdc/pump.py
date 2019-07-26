598cd5ece937d4d5ca37dc881ff867f6eb9a2f40
Brian DeCost <brian.decost@nist.gov>
2019-07-26 15:27:52 -0400

    start adding functionality for talking to multiple pumps.

--- asdc/sdc/pump.py
+++ asdc/sdc/pump.py
@@ -8,7 +8,15 @@ def encode(message):
 
 class PumpArray():
     """ KDS Legato pump array interface """
-    def __init__(self, port='COM7', baud=115200, timeout=1, output_buffer=100, fast=False):
+    def __init__(self, config=None, port='COM7', baud=115200, timeout=1, output_buffer=100, fast=False):
+        """ pump array.
+        What is needed? concentrations and flow rates.
+        Low level interface: set individual flow rates
+        High level interface: set total flow rate and composition
+        """
+        self.config = config
+
+        # serial interface things
         self.port = port
         self.baud = baud
         self.timeout = 1
@@ -22,7 +30,10 @@ class PumpArray():
             s = ser.read(100)
             print(s)
 
-    def eval(self, command, override=False):
+    def eval(self, command, pump_id=0, override=False):
+
+        command = '{} {}'.format(pump_id, command)
+
         if not override:
             if self.fast:
                 command = '@{}'.format(command)
@@ -36,45 +47,45 @@ class PumpArray():
         with serial.Serial(port=self.port, baudrate=self.baud, timeout=self.timeout) as ser:
             ser.write(encode(''))
 
-    def run(self):
-        self.eval('run')
+    def run(self, pump_id=0):
+        self.eval('run', pump_id=pump_id)
 
-    def stop(self):
-        self.eval('stop')
+    def stop(self, pump_id=0):
+        self.eval('stop', pump_id=pump_id)
 
-    def version(self, verbose=False):
+    def version(self, pump_id=0, verbose=False):
 
         if verbose:
-            self.eval('version')
+            self.eval('version', pump_id=pump_id)
         else:
-            self.eval('ver')
+            self.eval('ver', pump_id=0)
 
-    def address(self, idx=None):
+    def address(self, pump_id=0, new_id=None):
 
-        if idx is not None:
-            command = 'address {}'.format(idx)
+        if new_id is not None:
+            command = 'address {}'.format(new_id)
         else:
             command = 'address'
 
-        self.eval(command)
+        self.eval(command, pump_id=pump_id)
 
-    def crate(self):
-        self.eval('crate')
+    def crate(self, pump_id=0):
+        self.eval('crate', pump_id=pump_id)
 
-    def diameter(self, setpoint=None):
+    def diameter(self, pump_id=0, setpoint=None):
 
         if setpoint is not None:
             command = 'diameter {}'.format(setpoint)
         else:
             command = 'diameter'
 
-        self.eval(command)
+        self.eval(command, pump_id=pump_id)
 
-    def infusion_rate(self, rate=None, units='ml/min'):
+    def infusion_rate(self, pump_id=0, rate=None, units='ml/min'):
 
         if rate is not None:
             command = 'irate {} {}'.format(rate, units)
         else:
             command = 'irate'
 
-        self.eval(command)
+        self.eval(command, pump_id=pump_id)
