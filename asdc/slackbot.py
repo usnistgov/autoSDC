f94686f24bdb91de920b38b4051a8a915b704131
Brian DeCost <brian.decost@nist.gov>
2020-01-23 10:38:11 -0500

    pull in the CommandRegistry decorator from scirc and implement a decorated echo command to test slackclientv2

--- asdc/slackbot.py
+++ asdc/slackbot.py
@@ -12,13 +12,24 @@ bot_patterns = {
     'ctl': '<@UHNHM7198>'
 }
 
+class CommandRegistry(set):
+    """ wrap a set with a decorator to register commands """
+    def register(self, method):
+        self.add(method.__name__)
+        return method
+
+    def __call__(self, method):
+        """ overload __call__ instead of having an explicit register method """
+        return self.register(method)
+
 class SlackBot(object):
+    command = CommandRegistry()
 
     def __init__(self, name='sdc'):
         self.name = name
         # self._pattern = f'@{self.name}'
         self._pattern = bot_patterns.get(name) # '<@UHT11TM6F>'
-        self.command = {}
+        # self.command = {}
 
         slack.RTMClient.run_on(event='message')(self.handle_message)
 
@@ -56,13 +67,26 @@ class SlackBot(object):
                 )
 
             else:
-                msgdata = {'username': data['user'], 'channel': message['channel_id']}
-                self.log(text)
+                # msgdata = {'username': data['user'], 'channel': data['channel_id']}
+                # self.log(text)
 
                 # dispatch command method by name
                 # await getattr(self, command)(ws, msgdata, args)
-                await getattr(self, command)(web_client, args)
+                web_client = payload['web_client']
+                await getattr(self, command)(web_client, args, data)
 
+    @command
+    async def echo(self, web_client, text, data):
+        r = f"recieved command {text}"
+        channel = data.get('channel_id')
+        if channel is None:
+            channel = data.get('channel')
+        print(r)
+        web_client.chat_postMessage(
+            channel=channel,
+            text=r,
+            thread_ts=data['ts']
+        )
 
     async def main(self):
 
