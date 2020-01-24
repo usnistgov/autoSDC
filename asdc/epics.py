5926dffc133ddefb85ea3bc1df12314edef7ab75
Brian DeCost <brian.decost@nist.gov>
2020-01-23 20:26:26 -0500

    debug CA calls

--- asdc/epics.py
+++ asdc/epics.py
@@ -1,4 +1,5 @@
 import os
+import re
 import sys
 import shutil
 import typing
@@ -13,23 +14,28 @@ EPICS_LIB = SDC_LIB / EPICS_VERSION
 EPICS_EXTRA_BIN = pathlib.Path('home/phoebus/ANJ/win32/bin')
 sys.path.append(EPICS_LIB)
 
+GIT_BIN = pathlib.Path('/Program Files/Git/usr/bin').resolve()
+SCP = str(GIT_BIN / 'scp')
+
 print(EPICS_LIB)
 
-CA_GET = EPICS_LIB / 'caget.exe'
-CA_PUT = EPICS_LIB / 'caput.exe'
-CA_REPEATER = EPICS_LIB / EPICS_EXTRA_BIN / 'caRepeater.exe'
+CA_GET = str(EPICS_LIB / 'caget.exe')
+CA_PUT = str(EPICS_LIB / 'caput.exe')
+CA_REPEATER = str(EPICS_LIB / EPICS_EXTRA_BIN / 'caRepeater.exe')
 
 print(f"subprocess.Popen(['{CA_REPEATER}'])")
 if CA_REPEATER.is_file():
     subprocess.Popen([CA_REPEATER])
 
+
 def caget(key: str) -> str:
-    response = subprocess.check_output([CA_GET, key])
-    print(response)
-    return response
+    response = subprocess.check_output([CA_GET, key]).decode()
+    m = re.match(key, response)
+    start, end = m.span()
+    return response[end:].strip()
 
 def caput(key: str, value: str):
-    response = subprocess.check_output([CA_GET, key, value])
+    response = subprocess.check_output([CA_PUT, key, value])
     print(response)
     return response
 
@@ -39,4 +45,7 @@ def scp_get_files(pattern: str, remotehost: str = '6bm', dest='./'):
     make sure `remotehost` is included in `~/.ssh/config`, and that the
     `localhost` machine can use the `publickey` method to access `remotehost`
     """
-    subprocess.check_call(['scp', f'{remotehost}:{pattern}', dest])
+    print(SCP)
+    print(pattern)
+    print(f'{remotehost}:{pattern}')
+    subprocess.check_call([SCP, f'{remotehost}:{pattern}', dest])
