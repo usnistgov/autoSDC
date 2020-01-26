bugfix: incorrect computation of vector distance leads to the cell skipping a move if the target coordinates are swapped (x -> y and y -> x).

asdc/client.py
@@ -307,7 +307,7 @@ class SDC(slackbot.SlackBot):
         # map position update to position controller frame
         delta = self.compute_position_update(x, y, frame)
 
-        if np.abs(delta.sum()) > threshold:
+        if np.abs(delta).sum() > threshold:
             if self.verbose:
                 print(f'position update: {delta} (mm)')
 