d331903bf75233a2726f990e17db318f8417e0b3
Brian DeCost <brian.decost@nist.gov>
2020-08-13 17:18:47 -0400

    fix typo

--- asdc/localclient.py
+++ asdc/localclient.py
@@ -597,7 +597,7 @@ class SDC():
     def establish_droplet(self, x_wafer: float, y_wafer: float, flow_instructions: Dict = {}):
 
         target_rate = flow_instructions.get('flow_rate', 1.0)
-        purge_time = flow_intsructions.get('purge_time', 30)
+        purge_time = flow_instructions.get('purge_time', 30)
 
         # droplet workflow -- start at zero
         print('starting droplet workflow')
