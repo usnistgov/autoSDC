ed19db2f86b50e14ae12ef9855220e6979b84e04
Brian DeCost <brian.decost@nist.gov>
2020-08-24 17:08:53 -0400

    prototype pH meter

---
+++ asdc/sdc/orion.py
@@ -0,0 +1,144 @@
+import time
+import serial
+import typing
+import argparse
+import threading
+from contextlib import contextmanager
+
+def encode(message: str):
+    message = message + '\r'
+    return message.encode()
+
+class PHMeter():
+
+    supported_modes = {'pH', 'mV'}
+
+    def __init__(self, address, baud=9600, timeout=0.5, mode='pH'):
+
+        self.timeout = timeout
+
+        self.ser = serial.Serial(
+            port=address,
+            baudrate=baud,
+            parity=serial.PARITY_NONE,
+            bytesize=serial.EIGHTBITS,
+            stopbits=serial.STOPBITS_ONE,
+            timeout=timeout
+        )
+
+        self.mode = mode
+        self.blocking = False
+
+    @property
+    def mode(self):
+        return self._mode
+
+    @mode.setter
+    def mode(self, mode):
+        if mode in self.supported_modes:
+            self._mode = mode
+        else:
+            raise ValueError(f'meter mode must be one of {self.supported_modes}')
+
+        self.write(f'SETMODE {self.mode}')
+        return self.check_response()
+
+    @property
+    def blocking(self):
+        return self._blocking
+
+    @blocking.setter
+    def blocking(self, blocking: bool):
+        assert isinstance(blocking, bool)
+
+        if self._blocking == blocking:
+            pass
+        elif blocking:
+            self.ser.timeout = None
+        elif not blocking:
+            self.ser.timeout = self.timeout
+
+        self._blocking = blocking
+
+    @contextlib
+    def sync(self):
+        try:
+            self.blocking = True
+        finally:
+            self.blocking = False
+
+    def check_response(self):
+        response = self.ser.read(size=2).decode()
+        if response == '> ':
+            return True
+        else:
+            return False
+
+    def set_csv(self):
+        self.write('SETCSV')
+        return check_response()
+
+    def write(self, msg):
+        self.ser.write(encode(msg))
+
+    def _process_pH(self):
+        """
+        Meter Model, Serial Number, Software Revision, User ID, Date & Time, Sample ID, Channel, Mode, pH Value, pH Unit, mV Value, mV Unit, Temperature Value, Temperature Unit, Slope Value, Slope Unit, Method #, Log #
+        A211 pH, X01036, 3.04, ABCDE, 01/03/15 16:05:41, SAMPLE, CH-1, pH, 7.000, pH, 0.0, mV, 25.0, C, 98.1,%, M100, #1 <CR>
+        """
+
+        # get timestamp, sample_id, channel, mode, pH_value, pH_unit, mV_value, mV_unit, temperature_value, temperature_unit, slope_value, slope_unit
+
+        pass
+
+    def read(self, count: int = 1):
+
+        if count == 1:
+            self.write('GETMEAS')
+            line = self.ser.read_until(b'\r')
+            status = check_response()
+            print(line)
+
+        elif count > 1:
+            self.write(f'GETMEAS {count}')
+            lines = [self.ser.read_until(b'\r') for line in range(count)]
+            status = check_response()
+            print(lines)
+
+    def readloop(self, interval=30, logfile=None):
+
+        with self.sync():
+            while True:
+                target_ts = time.time() + interval
+
+                line = self.read(ensure_sync=True)
+
+                delta = target_ts - time.time()
+                time.sleep(max(0, delta))
+
+    @contextlib
+    def monitor(self, interval=30, logfile=None):
+        """ use this like
+
+        ```
+        with meter.monitor(interval=10):
+            time.sleep(60)
+        ```
+        """
+        io_worker = threading.Thread(target=readloop, args=(interval, logfile))
+        try:
+            io_worker.start()
+        finally:
+            io_worker.join()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='pH meter client')
+    parser.add_argument('--port',  default='COM21', help='COM port for the pH meter')
+    parser.add_argument('--verbose', action='store_true', help='include extra debugging output')
+    args = parser.parse_args()
+
+    meter = PHMeter(args.port)
+
+    with meter.monitor(interval=10):
+        time.sleep(60)
