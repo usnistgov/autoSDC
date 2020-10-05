ec00a037c5144cbf5564f32e09a06978304aac4f
Brian DeCost <brian.decost@nist.gov>
2020-10-05 15:17:08 -0400

    switch polarization_resistance to use a potential window defaulting to +/- 5mV instead of a current window

--- asdc/analysis/lpr.py
+++ asdc/analysis/lpr.py
@@ -9,8 +9,7 @@ from asdc._slack import SlackHandler
 
 logger = logging.getLogger(__name__)
 
-def current_crosses_zero(df):
-    current = df['current']
+def current_crosses_zero(current):
     success = current.min() < 0 and current.max() > 0
 
     if not success:
@@ -20,32 +19,41 @@ def current_crosses_zero(df):
 
     return success
 
-def polarization_resistance(df, current_window=2e-5):
-    current, potential = df['current'].values, df['potential'].values
+def polarization_resistance(current, potential, potential_window=0.005):
 
-    # slice out a symmetric window around zero current
-    window = (current > -current_window) & (current < current_window)
-    x, y = current[window], potential[window]
+    # find rough open circuit potential -- find zero crossing of current trace
+    # if the LPR fit is any good, then the intercept should give
+    # a more precise estimate of the open circuit potential
+    zcross = np.argmin(np.abs(current))
+    ocp = potential[zcross]
+
+    # select a window around OCP to fit
+    lb, ub = ocp - potential_window, ocp + potential_window
+    fit_p = (potential >= lb) & (potential <= ub)
 
     # quick linear regression
-    slope, intercept, r_value, p_value, std_err = stats.linregress(x, y)
+    slope, intercept, r_value, p_value, std_err = stats.linregress(current[fit_p], potential[fit_p])
 
-    return slope, intercept, r_value
+    r2 = r_value**2
 
-def lpr_analysis(df, lim=.005, r2thresh=.97):
+    return slope, intercept, r2
+
+def lpr_analysis(df, potential_window=.005, r2thresh=.97):
     current, potential = df['current'].values, df['potential'].values
 
     # find rough open circuit potential -- find zero crossing of current trace
+    # if the LPR fit is any good, then the intercept should give
+    # a more precise estimate of the open circuit potential
     zcross = np.argmin(np.abs(current))
     ocp = potential[zcross]
 
     # make sure there is at least on point in the scan on either side of OCP
-    flag1a = np.sum(potential > ocp+lim) > 1
-    flag1b = np.sum(potential < ocp-lim) > 1
+    flag1a = np.sum(potential > ocp + potential_window) > 1
+    flag1b = np.sum(potential < ocp - potential_window) > 1
     flag1 = flag1a and flag1b
 
     # select `lim` mV window around OCP to fit
-    fit_p = (potential > ocp-lim) & (potential < ocp+lim)
+    fit_p = (potential > ocp-potential_window) & (potential < ocp+potential_window)
 
     slope, intercept, r_value, p_value, std_err = stats.linregress(current[fit_p], potential[fit_p])
     flag2 = r_value**2 > r2thresh
@@ -70,7 +78,9 @@ class LPRData(EchemData):
         return 'LPR'
 
     def check_quality(self):
-        return current_crosses_zero(self)
+        slope, intercept, r2 = polarization_resistance(self.current, self.potential)
+        print(f'OCP: {intercept}')
+        return current_crosses_zero(self.current)
 
     def plot(self, fit=False):
         # # super().plot('current', 'potential')
