ed95f3f8822e76be7cd8dde00638daab709dceee
Brian DeCost <brian.decost@nist.gov>
2020-10-06 13:55:53 -0400

    add some logging to the flow mixing test script.

--- test/flow_mixing.py
+++ test/flow_mixing.py
@@ -2,8 +2,11 @@ import os
 import sys
 import json
 import time
+import serial
+import argparse
 import numpy as np
 from pathlib import Path
+from datetime import datetime
 
 project_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(project_dir)
@@ -17,9 +20,6 @@ solutions = {
     1: {'base': 1.0},
     2: {'neutral': 1.0}
 }
-
-phmeter = sdc.orion.PHMeter(ORION_PORT)
-pump_array = sdc.pump.PumpArray(solutions, port=PUMP_PORT, timeout=1)
 
 # what interface would be nice?
 # current:
@@ -48,16 +48,54 @@ S1 = {
     ]
 }
 
-data_dir = Path('pH-meter-test')
-os.makedirs(data_dir, exist_ok=True)
+def test_flow_mixing(data_dir, relative_rates, total_rate=11):
 
-total_rate = 11 # mL/min
-relative_rates = [0.001, 0.003, 0.1, 0.3, 1]
+    data_dir = Path(data_dir)
+    os.makedirs(data_dir, exist_ok=True)
 
-for idx, x in enumerate(relative_rates):
-    setpoint = {'acid': x * total_rate, 'base': (1-x) * total_rate}
+    phmeter = sdc.orion.PHMeter(ORION_PORT)
+    pump_array = sdc.pump.PumpArray(solutions, port=PUMP_PORT, timeout=1)
 
-    logfile = data_dir / f'pH-log-{idx}-x{x}.csv'
-    with self.phmeter.monitor(interval=1, logfile=logfile):
-        pump_arry.set_rates(setpoint, start=True, fast=True)
-        time.sleep(5*60)
+    meta = []
+    for idx, x in enumerate(relative_rates):
+        setpoint = {'acid': x * total_rate, 'base': (1-x) * total_rate}
+
+        logfile = f'pH-log-{idx}-x{x}.csv'
+        with phmeter.monitor(interval=1, logfile=data_dir/logfile):
+            pump_array.set_rates(setpoint, start=True, fast=True)
+            meta.append({'logfile': logfile, 'setpoint': setpoint, 'ts': datetime.now().isoformat()})
+            time.sleep(5*60)
+
+    with open(data_dir/'metadata.json') as f:
+        json.dump(meta, f)
+
+def dryrun(data_dir, relative_rates, total_rate=11):
+    data_dir = Path(data_dir)
+
+    meta = []
+    for idx, x in enumerate(relative_rates):
+        setpoint = {'acid': x * total_rate, 'base': (1-x) * total_rate}
+        logfile = f'pH-log-{idx}-x{x}.csv'
+        print(data_dir / logfile, setpoint)
+        meta.append({'logfile': logfile, 'setpoint': setpoint, 'ts': datetime.now().isoformat()})
+
+        time.sleep(3)
+
+    print(json.dumps(meta))
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='flow mixing test harness')
+    parser.add_argument('datadir', type=str, help='data and log directory')
+    parser.add_argument('--dashboard', action='store_true', help='set up ZMQ publisher for dashboard')
+    parser.add_argument('--dry-run', action='store_true', help='generate test output')
+    parser.add_argument('--verbose', action='store_true', help='include extra debugging output')
+    args = parser.parse_args()
+
+    total_rate = 11
+    relative_rates = [0, 0.001, 0.003, 0.1, 0.3, 1]
+
+    if args.dry_run:
+        dryrun(args.datadir, relative_rates, total_rate=total_rate)
+    else:
+        test_flow_mixing(args.datadir, relative_rates, total_rate)
